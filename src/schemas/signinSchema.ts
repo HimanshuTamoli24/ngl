2a0aad523ade51e884e5f38d10318ca86748e336
Himanshu Tamoli <himanshutamoli2005@gmail.com>
2025-08-06 23:35:39 +0530

    feat: simplify home page layout and update password validation length

--- src/schemas/signinSchema.ts
+++ src/schemas/signinSchema.ts
@@ -2,5 +2,5 @@ import { z } from "zod";
 
 export const signInSchema = z.object({
     email: z.string().email("Invalid email address"),
-    password: z.string().min(6, "Password must be at least 6 characters long").max(20, "Password must be at most 100 characters long"),
+    password: z.string().min(6, "Password must be at least 6 characters long").max(100, "Password must be at most 100 characters long"),
 })
