7883e2d5806a9e0d7cc3e4d66ffee5dc5f6e68d6
Himanshu Tamoli <himanshutamoli2005@gmail.com>
2025-08-08 22:50:43 +0530

    feat: create dashboard and user profile pages with basic layout

---
+++ src/app/(app)/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div className='w-full min-h-screen bg-green-400'>page</div>
+  )
+}
+
+export default page
---
+++ src/app/u/[username]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div className='w-full min-h-screen bg-green-400'>hello </div>
+  )
+}
+
+export default page
