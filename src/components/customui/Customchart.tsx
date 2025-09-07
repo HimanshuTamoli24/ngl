ea1ede154d33c782369030f09d2ab978d095cefb
Himanshu Tamoli <himanshutamoli2005@gmail.com>
2025-09-07 15:58:29 +0530

    feat: add custom chart components for message data visualization

---
+++ src/components/customui/Customchart.tsx
@@ -0,0 +1,47 @@
+import { AreaChart } from "@/components/retroui/charts/AreaChart";
+import { Message } from "../../models/user.model";
+import dayjs from "dayjs";
+
+
+export default function ChartStyleDefault({ messages }: { messages: Message[] }) {
+  const data = getMonthlyMessageData(messages);
+
+  return (
+    <AreaChart
+      data={data}
+      index="month"
+      categories={["count"]}
+    />
+  );
+}
+
+function getMonthlyMessageData(messages: Message[]) {
+  if (!messages || messages.length === 0) {
+    return generateYearSkeleton(new Date().getFullYear()); // 12 months, all 0
+  }
+
+  const currentYear = new Date().getFullYear();
+  const counts: Record<string, number> = {};
+
+  // Count only current year's messages
+  for (const msg of messages) {
+    const date = dayjs(msg.createdAt);
+    if (date.year() === currentYear) {
+      const key = date.format("MMM");
+     
+      counts[key] = (counts[key] || 0) + 1;
+    }
+  }
+
+  return generateYearSkeleton(currentYear).map(({ month }) => ({
+    month,
+    count: counts[month] || 0,
+  }));
+}
+
+function generateYearSkeleton(year: number) {
+  const months = Array.from({ length: 12 }, (_, i) =>
+    dayjs().year(year).month(i).format("MMM")
+  );
+  return months.map((m) => ({ month: m, count: 0 }));
+}
